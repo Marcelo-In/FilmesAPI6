973222d850827c7ecbd4bb34c6a9c1b0c7044f31
Marcelo <marceloinoue@hotmail.com>
2023-02-09 16:02:55 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI6/Controllers/FilmeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        
+    }
+}
